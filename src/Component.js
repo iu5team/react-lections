9faa62ba9f5b5e31279c11ebee303ef8136a506b
Sergey Chernobrovkin <sergey.chernobrovkin@inbox.ru>
2018-12-08 14:01:20 +0300

    Лекция 11

---
+++ src/Component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const Component = () => (<h1>Наше первое приложение</h1>);
+export default hot(module)(Component);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const build = path.resolve(__dirname, 'build');
+
+module.exports = {
+    entry: path.resolve(src, 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: build,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(src, 'index.html'),
+            filename: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    },
+    devServer: {
+        hot: true,
+        inline: true,
+        contentBase: false,
+    }
+};
